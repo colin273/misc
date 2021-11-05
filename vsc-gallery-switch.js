f3012377968ad6456da9dd80075c89ab1bc9d7c5
FifiTheBulldog <fifithebulldog@outlook.com>
2021-11-05 12:15:32 -0400

    Formatting for vsc-gallery-switch.js

--- vsc-gallery-switch.js
+++ vsc-gallery-switch.js
@@ -1,46 +1,46 @@
-const { writeFileSync } = require('fs');
+const { readFileSync, writeFileSync } = require('fs');
 
 const productPath = "/Applications/VSCodium.app/Contents/Resources/app/product.json";
 
 const openVSX = {
-    "serviceUrl": "https://open-vsx.org/vscode/gallery",
-    "itemUrl": "https://open-vsx.org/vscode/item"
+  "serviceUrl": "https://open-vsx.org/vscode/gallery",
+  "itemUrl": "https://open-vsx.org/vscode/item"
 };
 
 const microsoft = {
-    "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
-    "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
-    "itemUrl": "https://marketplace.visualstudio.com/items",
-    "controlUrl": "",
-    "recommendationsUrl": ""
+  "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
+  "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
+  "itemUrl": "https://marketplace.visualstudio.com/items",
+  "controlUrl": "",
+  "recommendationsUrl": ""
 };
 
-const doneMessage = "Set extension gallery source to ";
+const doneMessage = "Extensions gallery source set to ";
 
-let current = require(productPath);
+let current = JSON.parse(readFileSync(productPath, 'utf8'));
 
 switch (process.argv[2]) {
-    case "o":
-        current.extensionsGallery = openVSX;
-        writeFileSync(productPath, JSON.stringify(current, null, 2));
-        console.log(doneMessage + "Open VSX");
-        break;
-    case "m":
-        current.extensionsGallery = microsoft;    
-        writeFileSync(productPath, JSON.stringify(current, null, 2));
-        console.log(doneMessage + "Microsoft");
-        break;
-    default:
-        const currentGallery = current.extensionsGallery;
-        const currentProvider = (currentGallery.itemUrl === openVSX.itemUrl && currentGallery.serviceUrl === openVSX.serviceUrl) ? "Open VSX" : "Microsoft";
-        console.log(`
-        To change the VSCodium extension gallery source:
+  case "o":
+    current.extensionsGallery = openVSX;
+    writeFileSync(productPath, JSON.stringify(current, null, 2));
+    console.log(doneMessage + "Open VSX");
+    break;
+  case "m":
+    current.extensionsGallery = microsoft;    
+    writeFileSync(productPath, JSON.stringify(current, null, 2));
+    console.log(doneMessage + "Microsoft");
+    break;
+  default:
+    const currentGallery = current.extensionsGallery;
+    const currentProvider = (currentGallery.itemUrl === openVSX.itemUrl && currentGallery.serviceUrl === openVSX.serviceUrl) ? "Open VSX" : "Microsoft";
+    console.log(`
+To change the VSCodium extension gallery source:
 
-        - Open VSX: node ${__filename} o
-        - Microsoft: node ${__filename} m
+   - Open VSX: node ${__filename} o
+   - Microsoft: node ${__filename} m
 
-        Make sure to restart VSCodium for the changes to take effect.
+Make sure to restart VSCodium for the changes to take effect.
         
-        Current setting: ${currentProvider}
-        `);
+Current setting: ${currentProvider}
+`);
 }
